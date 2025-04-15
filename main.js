896fe2081697882d3b8b06e084ac469602c96bd9
7sn.dev <121363674+7snDev@users.noreply.github.com>
2025-04-16 02:52:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
+document.addEventListener("keydown", async (event) => {
+  if (event.key == "F12" || event.key == "F11" || event.key == "F5") {
+    // event.preventDefault();
+  }
+});
+
+if (window.sessionStorage.getItem("first") == null) {
+  window.sessionStorage.setItem("first", "true");
+  setTimeout(() => {
+    document.querySelector(".logo").remove();
+    document.querySelector(".text").remove();
+    document.querySelector(".prompt").style.visibility = "visible";
+    document.querySelector(".welcome").style.visibility = "visible";
+  }, 3500);
+} else {
+  document.querySelector(".logo").remove();
+  document.querySelector(".text").remove();
+  document.querySelector(".prompt").style.visibility = "visible";
+  document.querySelector(".welcome").style.visibility = "visible";
+}
